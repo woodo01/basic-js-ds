75f76f51fbcc0a70f3630c7c0142c5186d9db62f
Ivan Sigaev <ivan.develop@gmail.com>
2025-01-06 13:46:31 +0100

    Revert "test"
    This reverts commit 808cf4dd65330d18ad3c6abf9a11ccaac9d0ff3e.

--- test/binary-search-tree.test.js
+++ test/binary-search-tree.test.js
@@ -37,7 +37,7 @@ describe('Binary search tree', () => {
   describe('functional requirements', () => {
     it.optional('returns correct root if no value added', () => {
       const tree = new BinarySearchTree();
-      assert.strictEqual(true, true);
+      assert.strictEqual(tree.root(), null);
     });
 
     it.optional('root works correctly some values were added', () => {
@@ -45,7 +45,7 @@ describe('Binary search tree', () => {
       tree.add(2);
       tree.add(3);
       tree.add(4);
-      assert.strictEqual(true, true);
+      assert.strictEqual(tree.root().data, 2);
     });
 
     it.optional('find existing values', () => {
@@ -58,11 +58,10 @@ describe('Binary search tree', () => {
       tree.add(32);
       tree.add(12);
       tree.add(14);
-      assert.strictEqual(true, true);
-      assert.strictEqual(true, true);
-      assert.strictEqual(true, true);
-      assert.strictEqual(true, true);
-
+      assert.strictEqual(tree.find(8).data, 8);
+      assert.strictEqual(tree.find(2).data, 2);
+      assert.strictEqual(tree.find(32).data, 32);
+      assert.strictEqual(tree.find(14).data, 14);
     });
 
     it.optional('find non-existing values', () => {
@@ -75,10 +74,9 @@ describe('Binary search tree', () => {
       tree.add(32);
       tree.add(12);
       tree.add(14);
-      assert.strictEqual(true, true);
-      assert.strictEqual(true, true);
-      assert.strictEqual(true, true);
-      assert.strictEqual(true, true);
+      assert.strictEqual(tree.find(33), null);
+      assert.strictEqual(tree.find(1337), null);
+      assert.strictEqual(tree.find(42), null);
     });
 
     it.optional('correctly checks presence of the values', () => {
@@ -91,10 +89,10 @@ describe('Binary search tree', () => {
       tree.add(8);
       tree.add(31);
       tree.add(1);
-      assert.strictEqual(true, true);
-      assert.strictEqual(true, true);
-      assert.strictEqual(true, true);
-      assert.strictEqual(true, true);
+      assert.strictEqual(tree.has(54), true);
+      assert.strictEqual(tree.has(8), true);
+      assert.strictEqual(tree.has(7), false);
+      assert.strictEqual(tree.has(4), false);
     });
 
     it.optional('remove & has works correctly', () => {
