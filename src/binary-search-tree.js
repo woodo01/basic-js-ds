50d73746d10b9c8430055df1e20c5a307e4faf25
Ivan Sigaev <ivan.develop@gmail.com>
2025-01-06 16:49:34 +0100

    feat: add binary search tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -7,40 +7,89 @@ const { NotImplementedError } = require('../extensions/index.js');
 * using Node from extensions
 */
 class BinarySearchTree {
+  data  = null;
+  right = null;
+  left = null;
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.data == null) return null;
+    return this;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(value) {
+    if (this.data === null) {
+      this.data = value;
+      return null;
+    }
+    if (this.data > value) {
+      if (this.left === null) this.left = new BinarySearchTree();
+      this.left.add(value);
+      return null;
+    }
+    if (this.right === null) this.right = new BinarySearchTree();
+    this.right.add(value);
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(value) {
+    if (this.data === null) return false;
+    if (this.data === value) return true;
+    if (this.data > value) {
+      return this.left !== null && this.left.has(value);
+    }
+
+    return this.right !== null && this.right.has(value);
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(value) {
+    if (this.data === null) return null;
+    if (this.data === value) return this;
+    if (this.data > value) {
+      if (this.left === null) return null;
+      return this.left.find(value);
+    }
+
+    if (this.right === null) return null;
+    return this.right.find(value);
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(value) {
+    if (this.data === null) return null;
+    if (this.data === value) {
+      if (this.left !== null && this.right !== null) {
+        this.data = this.right.min();
+        return null;
+      }
+      if (this.left !== null) {
+        this.data = this.left.data;
+        this.left = this.left.left;
+        return null;
+      }
+      if (this.right !== null) {
+        this.data = this.right.data;
+        this.right = this.right.right;
+        return null;
+      }
+      this.data = null;
+      return null;
+    }
+
+    if (this.data > value) {
+      if (this.left === null) return null;
+      return this.left.remove(value);
+    }
+
+    if (this.right === null) return null;
+    return this.right.remove(value);
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.left === null) return this.data;
+    return this.left.min();
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.right === null) return this.data;
+    return this.right.max();
   }
 }
 
